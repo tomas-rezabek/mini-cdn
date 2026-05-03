0cafe62d14eeacd4a028c6c9a8b2a976465ec938
tomas <git@tomasrezabek.cz>
2026-05-03 16:36:16 +0200

    added url query and origin call with response

--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
 	
      http.HandleFunc("/proxy", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Mini CDN is running...")
+		// get url from the query
+		url := r.URL.Query().Get("url")
+
+		if url == "" {
+			http.Error(w, "Missing ?url", http.StatusBadRequest)
+			return
+		}
+
+		// call origin server
+		resp, err := http.Get(url)
+		if err != nil {
+			http.Error(w, "Failed to fetch origin", http.StatusBadGateway)
+			return
+		}
+		defer resp.Body.Close()
+
+		// setup content-type
+		w.Header().Set("Contenty-Type", resp.Header.Get("Content-Type"))
+
+		// send data to the client
+		io.Copy(w, resp.Body)
 	})
 
 	// run server on port 8080
