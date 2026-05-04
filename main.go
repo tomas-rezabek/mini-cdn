054f64799e72a00fd0a54a38a7ad89cc80d6ee9c
tomas-rezabek <git@tomasrezabek.cz>
2026-05-04 19:41:48 +0200

    implemented cache dir with HIT and MISS

--- main.go
+++ main.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+	"path/filepath"
 )
+
+const cacheDir = "cache"
+
+func cacheKey(url string) string {
+	hash := sha256.Sum256([]byte(url))
+	return hex.EncodeToString(hash[:])
+}
+
+func cachePath(url string) string {
+	return filepath.Join(cacheDir, cacheKey(url))
+}
 
 func main() {
 	
@@ -17,7 +32,25 @@ func main() {
 			return
 		}
 
-		// call origin server
+		os.MkdirAll(cacheDir, 0755)
+		filePath := cachePath(url)
+
+		// CACHE HIT
+		if _, err := os.Stat(filePath); err == nil {
+			w.Header().Set("X-Cache", "HIT")
+
+			file, err := os.Open(filePath)
+			if err != nil {
+				http.Error(w, "Failed to read cache", http.StatusInternalServerError)
+				return
+			}
+			defer file.Close()
+
+			io.Copy(w, file)
+			return
+		}
+
+		// CACHE MISS
 		resp, err := http.Get(url)
 		if err != nil {
 			http.Error(w, "Failed to fetch origin", http.StatusBadGateway)
@@ -25,11 +58,21 @@ func main() {
 		}
 		defer resp.Body.Close()
 
-		// setup content-type
-		w.Header().Set("Contenty-Type", resp.Header.Get("Content-Type"))
+		cacheFile, err := os.Create(filePath)
+		if err != nil {
+			http.Error(w, "Failed to create cache file", http.StatusInternalServerError)
+			return
+		}
+		defer cacheFile.Close()
 
-		// send data to the client
-		io.Copy(w, resp.Body)
+		// setup cache header
+		w.Header().Set("X-Cache", "MISS")
+		// setup content-type
+		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+
+		// sending data to cache and response
+		multiWriter := io.MultiWriter(w, cacheFile)
+		io.Copy(multiWriter, resp.Body)
 	})
 
 	// run server on port 8080
